4bd7bf104e0f9a24226ed48ce23610aea0991fcd
nakox0706 <nakox0706@gmail.com>
2018-06-24 22:19:29 +0900

    ver1.1

--- chat_line_css/function.php
+++ chat_line_css/function.php
@@ -1,10 +1,11 @@
 
 //////////////////////////////////////////////////
-//ショートコードで囲んだ範囲のpタグを消す
+//ショートコードで囲んだ範囲のp,&nbspタグを消す
 //////////////////////////////////////////////////
 function noFunc( $atts, $html = null ) {
 	$html = str_replace( '<p>' , '' , $html );
-  $html = str_replace( '</p>' , '' , $html );
-  return $html;
+	$html = str_replace( '</p>' , '' , $html );
+	$html = str_replace( '&nbsp' , '' , $html );
+	return $html;
 }
 add_shortcode('no', 'noFunc');
