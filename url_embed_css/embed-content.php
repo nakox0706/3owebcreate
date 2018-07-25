b330964e2702789149e106b77fcb921e0bdddd41
okamot <h.aoki@wdch.jp>
2018-07-25 13:53:09 +0900

    再度整形

--- url_embed_css/embed-content.php
+++ url_embed_css/embed-content.php
@@ -12,11 +12,16 @@
 ?>
 <style>
 
+a {
+  text-decoration: none;
+}
+
 .wp-embed {
   padding: 3% 3% 1% 3%;
   line-height: 120%;
-  font-size: 12px;
+  font-size: 14px;
 }
+
 
 .wp-embed-featured-image {
     width: 40%;
@@ -35,6 +40,7 @@
 	font-size: 120%;
   font-weight: bold ;
   color: #333 ;
+  line-height: 120%;
 	display: block;
 }
 
@@ -45,7 +51,7 @@
 }
 
 .wp-embed-footer {
-	padding-top: 10px;
+	padding-top: 0px;
   margin-top: 0;
 }
 
@@ -56,133 +62,165 @@
   color: #333 ;
 }
 
+.wp-embed__sp {
+  padding: 0;
+  line-height: 120%;
+  font-size: 12px;
+  display: table;
+  border: solid 1px #ccc;
+
+}
+
+.wp-embed__sp img{
+  width: 100%;
+  height: auto;
+  vertical-align: top;
+}
+
+.wp-embed__sp-featured-image {
+    display: table-cell;
+    width: 50%;
+}
+
+.wp-embed__sp-heading {
+	display: table-cell;
+  vertical-align: middle;
+  font-weight: bold ;
+  color: #333 ;
+  width: 50%;
+  line-height: 120%;
+  padding:1% 1% 1% 2%;
+}
+
+.wp-embed__sp-heading span{
+  display: block;
+  margin-top: 5px;
+  color: #ddd;
+  font-size: 80%;
+}
+
 </style>
 
-	<div <?php post_class( 'wp-embed' ); ?>>
-		<?php
-		$thumbnail_id = 0;
+<?php
+$thumbnail_id = 0;
 
-		if ( has_post_thumbnail() ) {
-			$thumbnail_id = get_post_thumbnail_id();
-		}
+if ( has_post_thumbnail() ) {
+  $thumbnail_id = get_post_thumbnail_id();
+}
 
-		if ( 'attachment' === get_post_type() && wp_attachment_is_image() ) {
-			$thumbnail_id = get_the_ID();
-		}
+if ( 'attachment' === get_post_type() && wp_attachment_is_image() ) {
+  $thumbnail_id = get_the_ID();
+}
 
-		/**
-		 * Filters the thumbnail image ID for use in the embed template.
-		 *
-		 * @since 4.9.0
-		 *
-		 * @param int $thumbnail_id Attachment ID.
-		 */
-		$thumbnail_id = apply_filters( 'embed_thumbnail_id', $thumbnail_id );
+$thumbnail_id = apply_filters( 'embed_thumbnail_id', $thumbnail_id );
 
-		if ( $thumbnail_id ) {
-			$aspect_ratio = 1;
-			$measurements = array( 1, 1 );
-			$image_size   = 'full'; // Fallback.
+if ( $thumbnail_id ) {
+  $aspect_ratio = 1;
+  $measurements = array( 1, 1 );
+  $image_size   = 'full'; // Fallback.
 
-			$meta = wp_get_attachment_metadata( $thumbnail_id );
-			if ( ! empty( $meta['sizes'] ) ) {
-				foreach ( $meta['sizes'] as $size => $data ) {
-					if ( $data['height'] > 0 && $data['width'] / $data['height'] > $aspect_ratio ) {
-						$aspect_ratio = $data['width'] / $data['height'];
-						$measurements = array( $data['width'], $data['height'] );
-						$image_size   = $size;
-					}
-				}
-			}
-
-			/**
-			 * Filters the thumbnail image size for use in the embed template.
-			 *
-			 * @since 4.4.0
-			 * @since 4.5.0 Added `$thumbnail_id` parameter.
-			 *
-			 * @param string $image_size   Thumbnail image size.
-			 * @param int    $thumbnail_id Attachment ID.
-			 */
-			$image_size = apply_filters( 'embed_thumbnail_image_size', $image_size, $thumbnail_id );
-
-			$shape = $measurements[0] / $measurements[1] >= 1.75 ? 'rectangular' : 'square';
-
-			/**
-			 * Filters the thumbnail shape for use in the embed template.
-			 *
-			 * Rectangular images are shown above the title while square images
-			 * are shown next to the content.
-			 *
-			 * @since 4.4.0
-			 * @since 4.5.0 Added `$thumbnail_id` parameter.
-			 *
-			 * @param string $shape        Thumbnail image shape. Either 'rectangular' or 'square'.
-			 * @param int    $thumbnail_id Attachment ID.
-			 */
-			$shape = apply_filters( 'embed_thumbnail_image_shape', $shape, $thumbnail_id );
-		}
-
-		if ( $thumbnail_id && 'rectangular' === $shape ) : ?>
-			<div class="wp-embed-featured-image">
-				<a href="<?php the_permalink(); ?>" target="_top">
-					<?php echo wp_get_attachment_image( $thumbnail_id, $image_size ); ?>
-				</a>
-			</div>
-		<?php endif; ?>
-
-		<div class="wp-embed-heading">
-			<a href="<?php the_permalink(); ?>" target="_top">
-				<?php the_title(); ?>
-			</a>
-      <?php
-      $ua = $_SERVER['HTTP_USER_AGENT'];
-      if (!preg_match('/(iPhone|Android.*Mobile|Windows.*Phone)/', $ua)) {
-        ?>
-          <div class="wp-embed-excerpt"><?php echo mb_substr(str_replace(array("\r\n", "\r", "\n"), '', strip_tags(get_the_excerpt())), 0, 400) ?></div>
-        <?php
+  $meta = wp_get_attachment_metadata( $thumbnail_id );
+  if ( ! empty( $meta['sizes'] ) ) {
+    foreach ( $meta['sizes'] as $size => $data ) {
+      if ( $data['height'] > 0 && $data['width'] / $data['height'] > $aspect_ratio ) {
+        $aspect_ratio = $data['width'] / $data['height'];
+        $measurements = array( $data['width'], $data['height'] );
+        $image_size   = $size;
       }
+    }
+  }
+
+  $image_size = apply_filters( 'embed_thumbnail_image_size', $image_size, $thumbnail_id );
+  $shape = $measurements[0] / $measurements[1] >= 1.75 ? 'rectangular' : 'square';
+  $shape = apply_filters( 'embed_thumbnail_image_shape', $shape, $thumbnail_id );
+}
+
+  $ua = $_SERVER['HTTP_USER_AGENT'];
+  if (preg_match('/(iPhone|Android.*Mobile|Windows.*Phone)/', $ua)) {
+    ?>
+    <a href="<?php the_permalink(); ?>" target="_top">
+    <div class="wp-embed__sp">
+      <?php
+      if ( $thumbnail_id && 'rectangular' === $shape ) : ?>
+        <div class="wp-embed__sp-featured-image">
+            <?php echo wp_get_attachment_image( $thumbnail_id, $image_size ); ?>
+
+        </div>
+      <?php endif; ?>
+      <div class="wp-embed__sp-heading">
+          <?php the_title(); ?>
+
+          <span>3oWebCreate</span>
+      </div>
+
+      <?php if ( $thumbnail_id && 'square' === $shape ) : ?>
+        <div class="wp-embed__sp-featured-image square">
+            <?php echo wp_get_attachment_image( $thumbnail_id, $image_size ); ?>
+        </div>
+      <?php endif; ?>
+      <?php
+      /**
+       * Prints additional content after the embed excerpt.
+       *
+       * @since 4.4.0
+       */
+      do_action( 'embed_content' );
+      ?>
+    </div>
+    </a>
+    <?php
+  }else {
+    ?>
+    <div <?php post_class( 'wp-embed' ); ?>>
+      <?php
+      if ( $thumbnail_id && 'rectangular' === $shape ) : ?>
+        <div class="wp-embed-featured-image">
+          <a href="<?php the_permalink(); ?>" target="_top">
+            <?php echo wp_get_attachment_image( $thumbnail_id, $image_size ); ?>
+          </a>
+        </div>
+      <?php endif; ?>
+
+      <div class="wp-embed-heading">
+        <a href="<?php the_permalink(); ?>" target="_top">
+          <?php the_title(); ?>
+        </a>
+            <div class="wp-embed-excerpt"><?php echo mb_substr(str_replace(array("\r\n", "\r", "\n"), '', strip_tags(get_the_excerpt())), 0, 150) ?></div>
+      </div>
+
+      <?php if ( $thumbnail_id && 'square' === $shape ) : ?>
+        <div class="wp-embed-featured-image square">
+          <a href="<?php the_permalink(); ?>" target="_top">
+            <?php echo wp_get_attachment_image( $thumbnail_id, $image_size ); ?>
+          </a>
+              <div class="wp-embed-excerpt"><?php echo mb_substr(str_replace(array("\r\n", "\r", "\n"), '', strip_tags(get_the_excerpt())), 0, 150) ?></div>
+        </div>
+      <?php endif; ?>
+      <?php
+      /**
+       * Prints additional content after the embed excerpt.
+       *
+       * @since 4.4.0
+       */
+      do_action( 'embed_content' );
       ?>
 
-		</div>
+      <div class="wp-embed-footer">
+        <?php the_embed_site_title() ?>
 
-		<?php if ( $thumbnail_id && 'square' === $shape ) : ?>
-			<div class="wp-embed-featured-image square">
-				<a href="<?php the_permalink(); ?>" target="_top">
-					<?php echo wp_get_attachment_image( $thumbnail_id, $image_size ); ?>
-				</a>
-        <?php
-        $ua = $_SERVER['HTTP_USER_AGENT'];
-        if (!preg_match('/(iPhone|Android.*Mobile|Windows.*Phone)/', $ua)) {
-          ?>
-            <div class="wp-embed-excerpt"><?php echo mb_substr(str_replace(array("\r\n", "\r", "\n"), '', strip_tags(get_the_excerpt())), 0, 400) ?></div>
+        <div class="wp-embed-meta">
           <?php
-        }
-        ?>
-			</div>
-		<?php endif; ?>
-		<?php
-		/**
-		 * Prints additional content after the embed excerpt.
-		 *
-		 * @since 4.4.0
-		 */
-		do_action( 'embed_content' );
-		?>
+          /**
+           * Prints additional meta content in the embed template.
+           *
+           * @since 4.4.0
+           */
+          do_action( 'embed_content_meta');
+          ?>
+        </div>
+      </div>
+    </div>
 
-		<div class="wp-embed-footer">
-			<?php the_embed_site_title() ?>
-
-			<div class="wp-embed-meta">
-				<?php
-				/**
-				 * Prints additional meta content in the embed template.
-				 *
-				 * @since 4.4.0
-				 */
-				do_action( 'embed_content_meta');
-				?>
-			</div>
-		</div>
-	</div>
-<?php
+    <?php
+  }
+  ?>
